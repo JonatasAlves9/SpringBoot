7ca4b0ddb2cce31c04449c413672aa90492131a8
Jonatas Alves <alvesjonatas99@gmail.com>
2020-08-07 17:18:50 -0300

    Criando Repository e Services para categoria

---
+++ src/main/java/com/curso/curso/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.curso.curso.services;
+
+import java.util.Optional;
+
+import com.curso.curso.domain.Categoria;
+import com.curso.curso.repositories.CategoriaRepository;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriaService {
+    
+    @Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+       
+}
